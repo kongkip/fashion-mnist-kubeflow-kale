9377b20bc8a987fe78ee8a01e8ccfeac6ed60932
kongkip <evanskiplagat3@gmail.com>
2022-03-19 13:44:11 +0300

    added kubeflow kale

--- main.py
+++ main.py
@@ -17,18 +17,18 @@ def ml_pipeline(epochs):
     dataset = Dataset()
     train_data, test_data, class_names = dataset.load_data()
 
-    # get the model
-    model = get_model()
-
-    # train the model
-    trained_model, history = train(model, train_data, test_data, epochs=epochs)
-
-    # evaluate the model
-    evaluate(trained_model, test_data)
-
-    # plot results and predictions
-    plot_results(history, epochs=epochs)
-    plot_predictions(trained_model, test_data, class_names)
+    # # get the model
+    # model = get_model()
+    #
+    # # train the model
+    # trained_model, history = train(model, train_data, test_data, epochs=epochs)
+    #
+    # # evaluate the model
+    # evaluate(trained_model, test_data)
+    #
+    # # plot results and predictions
+    # plot_results(history, epochs=epochs)
+    # plot_predictions(trained_model, test_data, class_names)
 
 
 if __name__ == '__main__':
